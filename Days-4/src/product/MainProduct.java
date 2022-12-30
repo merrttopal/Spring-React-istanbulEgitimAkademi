9306d3fa89fb73986dcce5df8b5bab06214dfe41
Mert Topal <87638248+merrttopal@users.noreply.github.com>
2022-12-30 23:00:58 +0300

    Day-4
    Access Modifiers

---
+++ Days-4/src/product/MainProduct.java
@@ -0,0 +1,11 @@
+package product;
+
+import works.Action;
+
+public class MainProduct {
+    public static void main(String[] args) {
+        Action action = new Action();
+        System.out.println(action.name);
+        //System.out.println(action.age); // defauld erişim anaharı olduğu için erişemez
+    }
+}
