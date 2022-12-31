3dc85e28f5de22ec748fbba3584df4847aa31453
Mert Topal <87638248+merrttopal@users.noreply.github.com>
2022-12-31 16:20:23 +0300

    Day-5-Overloading

---
+++ Days-5/src/overLoad/Action.java
@@ -0,0 +1,29 @@
+package overLoad;
+
+public class Action {
+
+
+    public void call(){
+
+    }
+
+
+    public int call(int num1){
+        return num1 + 1;
+    }
+
+    public int call(byte num1){
+        return num1 + 2;
+    }
+    public double call(float num1){
+        return num1 + 3;
+    }
+    public double call(double num1){
+        return num1 + 4;
+    }
+
+
+
+
+
+}
---
+++ Days-5/src/works/Setting.java
@@ -0,0 +1,9 @@
+package works;
+
+public class Setting {
+
+    public Setting() {
+        System.out.println("Settings Call");
+    }
+
+}
