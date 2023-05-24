b2e56d39f04aae7351d64accf475d73385a95b28
Mert Topal <87638248+merrttopal@users.noreply.github.com>
2023-05-24 23:33:49 +0300

    Jpa - Image Display

--- JPA/src/main/java/com/works/jpa/controllers/ImagesController.java
+++ JPA/src/main/java/com/works/jpa/controllers/ImagesController.java
@@ -3,6 +3,7 @@ package com.works.jpa.controllers;
 import com.works.jpa.entities.ProductImage;
 import com.works.jpa.service.ImagesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,17 +13,21 @@ import javax.sql.rowset.serial.SerialBlob;
 import java.io.IOException;
 import java.sql.Blob;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class ImagesController {
     Long pid = 0l;
     final ImagesService imageService;
+    List<ProductImage> ls = new ArrayList<>();
 
     @GetMapping("/images/{pid}")
     public String images(@PathVariable Long pid, Model model) {
         this.pid = pid;
-        model.addAttribute("images", imageService.list(this.pid));
+        ls = imageService.list(this.pid);
+        model.addAttribute("images", ls );
         return "images";
     }
 
@@ -36,4 +41,14 @@ public class ImagesController {
         imageService.addImage(productImage);
         return "redirect:/images/"+this.pid;
     }
+
+    @ResponseBody
+    @GetMapping (value = "/getImage/{index}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] getImage( @PathVariable int index ) throws IOException, SQLException {
+        Blob blob = ls.get(index).getImage();
+        int blobLength = (int) blob.length();
+        byte[] image = blob.getBytes(1, blobLength);
+        return image;
+    }
+
 }
