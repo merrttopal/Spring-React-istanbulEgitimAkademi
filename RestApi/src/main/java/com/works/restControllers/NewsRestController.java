9f3dce5a0b44f1d3d28a44972d39cfdac31660f3
Mert Topal <87638248+merrttopal@users.noreply.github.com>
2023-06-13 21:59:25 +0300

    RestApi/Session-Control

--- RestApi/src/main/java/com/works/restControllers/NewsRestController.java
+++ RestApi/src/main/java/com/works/restControllers/NewsRestController.java
@@ -1,20 +1,25 @@
 package com.works.restControllers;
 
 import com.works.Models.MusicCategoryList;
+import com.works.Models.NewsItem;
 import com.works.service.MozartService;
+import com.works.service.NewsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/mozart")
-public class MozartRestController {
-    final MozartService mozartService;
+@RequestMapping("/news")
+public class NewsRestController {
+    final NewsService newsService;
 
     @GetMapping("/service")
-    public MusicCategoryList service(){
-        return mozartService.result();
+    public List<NewsItem> news(){
+
+        return  newsService.news();
     }
 }
