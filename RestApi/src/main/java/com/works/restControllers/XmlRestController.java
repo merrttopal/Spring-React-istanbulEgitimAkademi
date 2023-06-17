a4add4d54b2b2e527e54e5d2125fcf1f6dfdcf5c
Mert Topal <87638248+merrttopal@users.noreply.github.com>
2023-06-17 17:48:33 +0300

    Xml Read V2

--- RestApi/src/main/java/com/works/restControllers/XmlRestController.java
+++ RestApi/src/main/java/com/works/restControllers/XmlRestController.java
@@ -1,6 +1,10 @@
 package com.works.restControllers;
 
+import com.works.Models.CdModel;
+import com.works.Models.Plant;
 import com.works.Models.XmlCurrency;
+import com.works.service.CdService;
+import com.works.service.PlantService;
 import com.works.service.XmlRead;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +18,25 @@ import java.util.List;
 @RequestMapping("/xml")
 public class XmlRestController {
     final XmlRead xmlRead;
+    final PlantService plantService;
+    final CdService cdService;
 
     @GetMapping("/read")
     public List<XmlCurrency>  read(){
        List<XmlCurrency> ls = xmlRead.result();
         return  ls;
+    }
+
+    @GetMapping("/plants")
+    public List<Plant> plants(){
+        List<Plant> list = plantService.plantList();
+        return list;
+    }
+
+    @GetMapping("/cd")
+    public List<CdModel> cdList(){
+        List<CdModel> cdList = cdService.cdModelList();
+        return cdList;
     }
 
 
