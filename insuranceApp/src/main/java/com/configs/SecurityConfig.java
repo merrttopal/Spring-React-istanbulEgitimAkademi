5b14acfd4bcc8f4704ad0b19a7a0d6631dca5572
Mert Topal <87638248+merrttopal@users.noreply.github.com>
2023-07-26 22:57:24 +0300

    Security config

---
+++ insuranceApp/src/main/java/com/configs/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .formLogin()
+                .disable();
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
---
+++ insuranceApp/src/main/java/com/configs/entities/User.java
@@ -0,0 +1,32 @@
+package com.configs.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long uid;
+
+
+    @Email
+    @NotBlank
+    @NotEmpty
+    @NotNull
+    private String mail;
+
+
+    @Size(min = 6, max = 20)
+    @NotBlank
+    @NotEmpty
+    @NotNull
+    private String password;
+
+
+
+}
